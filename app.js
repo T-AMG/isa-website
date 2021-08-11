56ed6373fb31e4cf6452bafed2eb56ba615684fb
Theo Abossolo <theo.abossolo@gmail.com>
2021-08-11 20:50:39 +0100

    index page finished

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const { config } = require("dotenv");
-
-const port = process.env.PORT || 3000;
-
 const express = require("express");
 var path = require("path");
 
+const routes = require("./routes/routes");
+
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 //view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -15,11 +16,10 @@ app.set("view engine", "ejs");
 /*app.use(express.json); */
 app.use(express.urlencoded({ extended: false }));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.send("Hello world!!!");
-});
+// Web sites routes
+app.use("/", routes);
 
 app.listen(port, () => {
   console.log(`Application Started at port: ${port}...`);
